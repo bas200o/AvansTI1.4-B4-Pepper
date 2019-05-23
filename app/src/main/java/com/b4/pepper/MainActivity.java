7ff43f3ce92fb2c1e800356609c07b078c3332f7
Wessel <wesselvanz@gmail.com>
2019-05-23 11:35:43 +0200

    added hello Human (doesn't work)

--- app/src/main/java/com/b4/pepper/MainActivity.java
+++ app/src/main/java/com/b4/pepper/MainActivity.java
@@ -3,11 +3,36 @@ package com.b4.pepper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.aldebaran.qi.sdk.QiContext;
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+import com.aldebaran.qi.sdk.builder.SayBuilder;
+import com.aldebaran.qi.sdk.object.conversation.Say;
+
+public class MainActivity extends AppCompatActivity implements RobotLifecycleCallbacks {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onRobotFocusGained(QiContext qiContext) {
+
+        System.out.println("Test");
+
+        Say say = SayBuilder.with(qiContext).withText("Hello human!").build();
+        say.run();
+    }
+
+    @Override
+    public void onRobotFocusLost() {
+
+    }
+
+    @Override
+    public void onRobotFocusRefused(String reason) {
+
     }
 }
