464fa25fbc912851c7fc1f7ffbc61d307fbd9885
Unknown <kenley.strik@casema.nl>
2019-06-06 14:11:15 +0200

    Changes to ui

--- app/src/main/java/com/b4/pepper/AppActivity.java
+++ app/src/main/java/com/b4/pepper/AppActivity.java
@@ -6,12 +6,19 @@ import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.aldebaran.qi.sdk.QiContext;
 import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
@@ -23,8 +30,8 @@ import com.b4.pepper.ui.main.SectionsPagerAdapter;
 
 public class AppActivity extends RobotActivity implements RobotLifecycleCallbacks, ISpeechToTextReceiver
 {
-    private ViewPager viewPager;
-    private TabLayout tabs;
+    private NonSwipeableViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -35,8 +42,8 @@ public class AppActivity extends RobotActivity implements RobotLifecycleCallback
 
         this.viewPager = findViewById(R.id.view_pager);
         this.viewPager.setAdapter(sectionsPagerAdapter);
-        this.tabs = findViewById(R.id.tabs);
-        this.tabs.setupWithViewPager(this.viewPager);
+        this.tabLayout = findViewById(R.id.tabs);
+        this.tabLayout.setupWithViewPager(this.viewPager);
 
 //        FloatingActionButton fab = findViewById(R.id.fab);
 //
@@ -47,6 +54,16 @@ public class AppActivity extends RobotActivity implements RobotLifecycleCallback
 //                        .setAction("Action", null).show();
 //            }
 //        });
+
+        LinearLayout tabStrip = ((LinearLayout)tabLayout.getChildAt(0));
+        for(int i = 0; i < tabStrip.getChildCount(); i++) {
+            tabStrip.getChildAt(i).setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    return true;
+                }
+            });
+        }
     }
 
     @Override
@@ -81,5 +98,62 @@ public class AppActivity extends RobotActivity implements RobotLifecycleCallback
     public void onAttachFragment(Fragment fragment)
     {
         super.onAttachFragment(fragment);
+    }
+
+    public void onWantsTableButton(View view)
+    {
+        this.tabLayout.getTabAt(1).select();
+    }
+
+    public void onPersonsButtonClicked(View view)
+    {
+        TextView errorMessage = findViewById(R.id.errorMessage);
+        String input = ((Button)view).getText().toString();
+        EditText personCountText = findViewById(R.id.personsCount);
+        String text = personCountText.getText().toString();
+
+        try
+        {
+            String newText = text + input;
+            int numberOfPersons = Integer.parseInt(newText);
+
+            if(numberOfPersons != 0 || text.length() > 0)
+            {
+                personCountText.setText(newText);
+            }
+
+            if(numberOfPersons > 40)
+            {
+                errorMessage.setText("Te veel personen");
+                errorMessage.setVisibility(View.VISIBLE);
+            }
+            else
+            {
+                errorMessage.setText("");
+                errorMessage.setVisibility(View.INVISIBLE);
+            }
+        } catch(Exception e) { }
+    }
+
+    public void onTableReserve(View view)
+    {
+        TextView errorMessage = findViewById(R.id.errorMessage);
+        EditText personCountText = findViewById(R.id.personsCount);
+        String text = personCountText.getText().toString();
+
+        try
+        {
+            int numberOfPersons = Integer.parseInt(text);
+
+            if(numberOfPersons <= 40)
+            {
+                this.tabLayout.getTabAt(2).select();
+            }
+        }
+        catch(Exception e)
+        {
+            errorMessage.setText("Incorrecte invoer");
+            errorMessage.setVisibility(View.VISIBLE);
+        }
     }
 }
