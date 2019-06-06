552630e51394d40be73d850e573c4e172493bd8e
Unknown <kenley.strik@casema.nl>
2019-06-06 16:40:08 +0200

    In app language change in progress

--- app/src/main/java/com/b4/pepper/AppActivity.java
+++ app/src/main/java/com/b4/pepper/AppActivity.java
@@ -1,20 +1,11 @@
 package com.b4.pepper;
 
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.view.DragEvent;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -27,6 +18,8 @@ import com.aldebaran.qi.sdk.object.conversation.ListenResult;
 import com.b4.pepper.model.ISpeechToTextReceiver;
 import com.b4.pepper.model.SpeechRecognizer;
 import com.b4.pepper.ui.main.SectionsPagerAdapter;
+
+import java.util.Locale;
 
 public class AppActivity extends RobotActivity implements RobotLifecycleCallbacks, ISpeechToTextReceiver
 {
@@ -217,5 +210,20 @@ public class AppActivity extends RobotActivity implements RobotLifecycleCallback
             errorMessage.setText("");
             errorMessage.setVisibility(View.INVISIBLE);
         }
+    }
+
+    public void onChangeLanguageClicked(View view)
+    {
+        System.out.println("CHANGE LANGUAGE CLICKED!");
+
+        if (Build.VERSION.SDK_INT >= 17)
+        {
+            getResources().getConfiguration().setLocale(new Locale("nl"));
+        }
+        else
+        {
+            getResources().getConfiguration().locale = new Locale("nl");
+        }
+        getResources().updateConfiguration(getResources().getConfiguration(), getResources().getDisplayMetrics());
     }
 }
