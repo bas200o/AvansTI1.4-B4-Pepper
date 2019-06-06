ff7ce4221146c9e4966be7bddf47a4f756f2ef3f
bas200o <bas.winkelhof@hotmail.nl>
2019-06-06 16:54:05 +0200

    update mqttarduino sketch

--- aruino/sketch_mqttultrasound/sketch_mqttultrasound.ino
+++ aruino/sketch_mqttultrasound/sketch_mqttultrasound.ino
@@ -1,67 +1,48 @@
-/*-------------------------------------------------------------------------
-
-Android to mqtt example for ESP32 based hardware. TI-1.4 Avans Breda
-
-copyright may, 2019 dm.kroeske@avans.nl
-
-Permission is hereby granted, free of charge, to any person obtaining a copy 
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights 
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is furnished
-to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in 
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR 
-IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
--------------------------------------------------------------------------*/
-
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 #include "WiFi.h"
 
+// CONFIG table
+/********************************************************************/
+const char* id          = "0";
+const char* seats       = "8";
+/*********************************************************************/
+
+// CONFIG Pins
+int ledredpin = 14;
+int ledgreenpin = 15;
+
+int trigPin = 13;
+int echoPin = 12;
+long pulse;
+int distance;
+
+// CONFIG WIFI
 const char* ssid        = "1";
 const char* password    = "12345678";
 
+
+//CONFIG MQTT
 const char* mqtt_broker   = "51.254.217.43";
-const char* mqtt_topic    = "TI-14-2019/LED";
+int         mqtt_port     = 1883;
+const char* mqtt_topic    = "dezeTopicNaamIsSpecifiekLangGemaaktOmAnderenWegTeHoudenVanOnzePrachtigePepperRobot";
 const char* mqtt_username = "emon";
 const char* mqtt_password = "uw2ELjAKrEUwqgLT";
-
-
-const int LED           = 14;  // Aanpassen!!
-const int BTN           = 0;  // Aanpassen!!
 
 WiFiClient wifiClient;
 PubSubClient mqttClient("", 0, wifiClient);
 
-/******************************************************************/
-void setup()
-/* 
-short:      ESP8266 (Arduino) setup
-inputs:        
-outputs: 
-notes:         
-Version :   DMK, Initial code
-*******************************************************************/
+void setup() 
 {
-
   // Init I/O devices
-  pinMode(LED, OUTPUT);
-  digitalWrite(LED, true);  // LED uit
-  pinMode(BTN, INPUT);
+  pinMode(ledredpin, OUTPUT);
+  pinMode(ledgreenpin, OUTPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 
   // Init serial port
-  Serial.begin(115200);
-  Serial.println("");
-  Serial.println("TI-1.4 MQTT example for ESP32");
+  Serial.begin(19200);
+  Serial.println("Pepper arduino comms via mqtt");
 
   // Enable WiFi en wacht op verbinding
   WiFi.mode(WIFI_STA);
@@ -75,57 +56,58 @@ Version :   DMK, Initial code
   Serial.print("");
   Serial.println("Wifi Connected: ");
   Serial.println(WiFi.localIP());
+  
 }
 
-/******************************************************************/
-void loop()
-/* 
-short:    ESP8266 (Arduino) main loop
-inputs:   
-outputs: 
-notes:    
-Version:  DMK, Initial code
-*******************************************************************/
+
+void loop() 
 {
+
+
   if (WiFi.status() == WL_CONNECTED) {
-    
     if( !mqttClient.connected() ) {
       mqtt_connect();
     } else {
       mqttClient.loop();
     }
-  
-    // Publish payload is BTN is pressed
-    if( !digitalRead(BTN) ) {
-      mqtt_pubish(
-        random(100, 255), // Red value
-        random(100, 255), // Green value
-        random(100, 255)  // Blue value
-      );
-      delay(200);
-    }
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  pulse = pulseIn(echoPin, HIGH);
+  distance = pulse / 29 / 2;
+  //Serial.println(distance);
+
+  if(distance < 80){
+    digitalWrite(ledredpin, true);
+    digitalWrite(ledgreenpin, false);
+    mqtt_pubish(false);
+  }else{
+    digitalWrite(ledgreenpin, true);
+    digitalWrite(ledredpin, false);
+    mqtt_pubish(true);
+  }
+
+    
     
   } else { 
     Serial.println("Geen WiFi verbinding !");
-    delay(1000);
+    delay(900);
   }
+  delay(1000);
 }
 
-/******************************************************************/
+
 void mqtt_connect() 
-/* 
-short:      Connect to MQTT server UNSECURE
-inputs:        
-outputs: 
-notes:         
-Version :   DMK, Initial code
-*******************************************************************/
-{  
+{
   mqttClient.setClient(wifiClient);
-  mqttClient.setServer(mqtt_broker, 1883);
+  mqttClient.setServer(mqtt_broker, mqtt_port);
 
   // Connect with unique id
-  String clientId = "TI14-";
+  String clientId = "PepperProject-";
   clientId += String(random(0xffff), HEX);
   clientId += '-';
   clientId += String((uint32_t)ESP.getEfuseMac(), HEX);
@@ -137,29 +119,22 @@ Version :   DMK, Initial code
 
     // Setup callback
     mqttClient.setCallback(mqtt_callback);
-    Serial.printf("%s: Connected to %s:%d\n", __FUNCTION__, mqtt_broker, 1883);
+    Serial.printf("%s: Connected to %s:%d\n", __FUNCTION__, mqtt_broker, mqtt_port);
   } else {    
-    Serial.printf("%s: Connection ERROR (%s:%d)\n", __FUNCTION__, mqtt_broker, 1883);
+    Serial.printf("%s: Connection ERROR (%s:%d)\n", __FUNCTION__, mqtt_broker, mqtt_port);
     delay(2000);
   }
 }
 
-/******************************************************************/
-void mqtt_pubish(int r, int g, int b)
-/* 
-short:      Pulish on MQTT topic (UNSECURE)
-inputs:        
-outputs: 
-notes:         
-Version :   DMK, Initial code
-*******************************************************************/
+
+void mqtt_pubish(boolean isAvailable)
 {
-  DynamicJsonDocument jsonDocument(1024);
+  DynamicJsonDocument jsonDocument(2048);
 
   JsonObject colors = jsonDocument.createNestedObject("ledColor");
-  colors["r"] = r;
-  colors["g"] = g;
-  colors["b"] = b;
+  colors["id"] = id;
+  colors["isAvailable"] = isAvailable;
+  colors["seats"] = seats;
 
   char json[1024];
   serializeJson(jsonDocument, json);
@@ -167,30 +142,23 @@ Version :   DMK, Initial code
   mqttClient.publish(mqtt_topic, json);
 }
 
-/******************************************************************/
+
 void mqtt_callback(char* topic, byte* payload, unsigned int length)
-/* 
-short:    MQTT callback. Elke publish op subscibed topic wordt hier
-          afgehandeld
-inputs:   'topic' waarop gepublished is
-          'payload' bevat de published datablock
-          'length' is de lengte van het payload array
-outputs: 
-notes:    In deze callback wordt gebruikt gemaakt van JSON parser
-Version:  DMK, Initial code
-*******************************************************************/
 {
-  if( 0 == strcmp(topic, mqtt_topic) ) {
+    if( 0 == strcmp(topic, mqtt_topic) ) {
     // Parse payload
     DynamicJsonDocument jsonDocument(1024);
+
+
+    
     DeserializationError error = deserializeJson(jsonDocument, payload);
     if( !error ) {
-      JsonVariant msg = jsonDocument["ledColor"];
+      JsonVariant msg = jsonDocument["esp"];
       if(!msg.isNull()) {
         // Flits de blauwe led
-        digitalWrite(LED, false);
+        digitalWrite(ledredpin, false);
         delay(50);
-        digitalWrite(LED, true);
+        digitalWrite(ledredpin, true);
       }
     }
   }
