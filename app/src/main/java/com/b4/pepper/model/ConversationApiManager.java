e33e759da45b530d457af7de26277c6093f9c20d
Boomkop3 <sjors.de.bruijn@gmail.com>
2019-06-18 14:32:25 +0200

    Added test return to conversation api manager

--- app/src/main/java/com/b4/pepper/model/ConversationApiManager.java
+++ app/src/main/java/com/b4/pepper/model/ConversationApiManager.java
@@ -14,6 +14,6 @@ public class ConversationApiManager {
     public int getTablesAvailable(int numberOfPeople) {
         // ToDo:
         // Check how many tables are available for the requested number of people
-        return 0;
+        return (numberOfPeople<10)?5:0;
     }
 }
