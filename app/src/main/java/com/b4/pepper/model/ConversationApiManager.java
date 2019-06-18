52ed5ca750e6f5d045821cd716d45680ebf042f0
Boomkop3 <sjors.de.bruijn@gmail.com>
2019-06-18 13:44:58 +0200

    Created empty ConversationAPIManager

---
+++ app/src/main/java/com/b4/pepper/model/ConversationApiManager.java
@@ -0,0 +1,9 @@
+package com.b4.pepper.model;
+
+public class ConversationApiManager {
+    public int getTablesAvailable(int numberOfPeople) {
+        // ToDo:
+        // Check how many tables are available for the requested number of people
+        return 0;
+    }
+}
