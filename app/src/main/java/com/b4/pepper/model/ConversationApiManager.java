5fdd275f4ee83a005aa0a0e0cbd7e5c6bcbc1272
Boomkop3 <sjors.de.bruijn@gmail.com>
2019-06-18 13:47:38 +0200

    Made ConversationApiManager Singleton

--- app/src/main/java/com/b4/pepper/model/ConversationApiManager.java
+++ app/src/main/java/com/b4/pepper/model/ConversationApiManager.java
@@ -1,6 +1,16 @@
 package com.b4.pepper.model;
 
 public class ConversationApiManager {
+    private ConversationApiManager instance;
+    public ConversationApiManager getInstance(){
+        if (instance == null){
+            instance = new ConversationApiManager();
+        }
+        return instance;
+    }
+    private ConversationApiManager(){
+        // SingleTon
+    }
     public int getTablesAvailable(int numberOfPeople) {
         // ToDo:
         // Check how many tables are available for the requested number of people
