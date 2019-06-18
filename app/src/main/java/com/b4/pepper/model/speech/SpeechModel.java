923084235fa69c59903bb0a3ae1174459a8a0383
Boomkop3 <sjors.de.bruijn@gmail.com>
2019-06-18 14:39:12 +0200

    Undid the last change

--- app/src/main/java/com/b4/pepper/model/speech/SpeechModel.java
+++ app/src/main/java/com/b4/pepper/model/speech/SpeechModel.java
@@ -1,6 +1,5 @@
 package com.b4.pepper.model.speech;
 
-import com.aldebaran.qi.Future;
 import com.aldebaran.qi.sdk.QiContext;
 import com.aldebaran.qi.sdk.builder.SayBuilder;
 import com.aldebaran.qi.sdk.object.conversation.Say;
@@ -24,13 +23,6 @@ public class SpeechModel {
     public void sayMessage(String message) {
 
         Say say = SayBuilder.with(this.qiContext).withText(message).build();
-        Future future = say.async().run();
-        try {
-            while (!future.isDone()){
-                Thread.sleep(50);
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        say.run();
     }
 }
