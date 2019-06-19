0646bdfa07fa8bde33e95b09266eab674dfba5bf
Wessel van Zeggeren <wessel.van.zeggeren@gmail.com>
2019-06-19 16:56:59 +0200

    Gebruik lambda's Sjors. Die zijn veel mooier en korter.
    Nee, die zitten niet in java 7. Waar android 6 officieel op draait.

--- app/src/main/java/com/b4/pepper/model/ThreadingHelper.java
+++ app/src/main/java/com/b4/pepper/model/ThreadingHelper.java
@@ -26,6 +26,7 @@ public class ThreadingHelper {
     }
 
     public static void runOffMainThreadSynchronous(Runnable toRun){
+
         Thread offMainThread = new Thread(toRun);
         offMainThread.start();
         try {
@@ -36,27 +37,23 @@ public class ThreadingHelper {
     }
 
     public static void stopChat() throws NullPointerException {
-        ThreadingHelper.runOffMainThreadSynchronous(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        if (chat == null) {
-                            Log.d("Stopping chat",  "chat is null");
-                        }
-                        if (chat != null && chatFuture != null){
-                            Log.d("Stopping chat",  "stopping...");
-                            chatFuture.cancel(true);
-                            chatFuture.requestCancellation();
-                            while (!chat.getSaying().getText().equals("")) {
-                                try {
-                                    Thread.sleep(10);
-                                } catch (InterruptedException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        }
+
+        ThreadingHelper.runOffMainThreadSynchronous(() -> {
+            if (chat == null) {
+                Log.d("Stopping chat",  "chat is null");
+            }
+            if (chat != null && chatFuture != null){
+                Log.d("Stopping chat",  "stopping...");
+                chatFuture.cancel(true);
+                chatFuture.requestCancellation();
+                while (!chat.getSaying().getText().equals("")) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
                 }
-        );
+            }
+        });
     }
 }
