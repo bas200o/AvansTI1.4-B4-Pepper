53bcd5b19786401f54d08945540df0465a526f08
Boomkop3 <sjors.de.bruijn@gmail.com>
2019-06-19 15:16:34 +0200

    Removed todo comment for completed task

--- app/src/main/java/com/b4/pepper/model/ConversationApiManager.java
+++ app/src/main/java/com/b4/pepper/model/ConversationApiManager.java
@@ -1,5 +1,8 @@
 package com.b4.pepper.model;
 
+import com.b4.pepper.model.entity.ESPEntity;
+import com.b4.pepper.model.mqtt.TableManager;
+
 public class ConversationApiManager {
     private static ConversationApiManager instance;
     public static ConversationApiManager getInstance(){
@@ -11,9 +14,21 @@ public class ConversationApiManager {
     private ConversationApiManager(){
         // SingleTon
     }
-    public int getTablesAvailable(int numberOfPeople) {
-        // ToDo:
-        // Check how many tables are available for the requested number of people
-        return (numberOfPeople<10)?5:0;
+
+    /**
+     * Finds and returns a table for the given number of people
+     * @param numberOfPeople the number of people that would like a single table
+     * @return the id of the found table. Or -1 if there's no table available
+     */
+    public int getTableForGuests(int numberOfPeople) {
+        TableManager tableManager = new TableManager();
+        tableManager.reserveTable(numberOfPeople);
+        ESPEntity table = tableManager.getPickedTable();
+        if (table != null){
+            return table.getId();
+        }
+        else {
+            return -1;
+        }
     }
 }
